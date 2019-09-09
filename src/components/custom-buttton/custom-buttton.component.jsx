8a257bb0a2b60f22c7c5cb56840081fafc2a9623
Glen Pegado <glenpegado@gmail.com>
2019-09-09 10:42:31 -0500

    connected button to sign in form, added custom desgin to button

--- src/components/custom-buttton/custom-buttton.component.jsx
+++ src/components/custom-buttton/custom-buttton.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './custom-buttton.component';
+import './custom-buttton.styles.scss';
 
 const CustomButton = ({children, ...otherProps}) => (
     <button className='custom-button' {...otherProps}>
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-buttton/custom-buttton.component';
 import './sign-in.styles.scss';
 
 
@@ -50,7 +51,7 @@ class SignIn extends React.Component{
                         required
                     />
     
-                    <input type="submit" value='Submit Form'/>
+                    <CustomButton type="submit" > Sign In</CustomButton>
                 </form>
             </div>
         )
