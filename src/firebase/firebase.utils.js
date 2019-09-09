55bb9522c1fd111ec0080e464f35befd98731cbf
Glen Pegado <glenpegado@gmail.com>
2019-09-09 11:47:58 -0500

    Users can now sign in using Google Auth

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,3 +14,15 @@ const config = {
 
 firebase.initializeApp(config);
 
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+//Google pop-up
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
+
+
+
