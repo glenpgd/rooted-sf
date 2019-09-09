67663bb028d0c30a3d8086fbd814516c614f764c
Glen Pegado <glenpegado@gmail.com>
2019-09-09 14:06:06 -0500

    created sign out feature, and styled sign in button

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 
+import {auth} from '../../firebase/firebase.utils'
+
 import './header.styles.scss';
 
-const Header = () => {
+const Header = ({currentUser}) => {
     return (
         <div className='header'>
-
            <Link className='logo-container' to='/'>
                 <Logo className='logo'/>
            </Link>
@@ -20,6 +21,18 @@ const Header = () => {
                 <Link className='option' to='/shop'>
                     CONTACT
                 </Link>
+                
+                {
+                    currentUser ?
+                    <div 
+                        className='option' 
+                        onClick={() => auth.signOut()}> SIGN OUT
+                    </div>
+                    :
+                    <Link className='option' to='/signin'>
+                        SIGN IN
+                    </Link>
+                }
            </div>
         </div>
     )
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
-import CustomButton from '../custom-buttton/custom-buttton.component';
+import CustomButton from '../custom-button/custom-button.component';
 import {signInWithGoogle} from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
@@ -51,10 +51,13 @@ class SignIn extends React.Component{
                         label='password'
                         required
                     />
+
+                    <div className='buttons'>
+                        <CustomButton type="submit" > Sign In</CustomButton> 
+                        
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign In With Google</CustomButton>
+                    </div>
     
-                    <CustomButton type="submit" > Sign In</CustomButton>
-                    
-                    <CustomButton onClick={signInWithGoogle} > Sign In With Google</CustomButton>
                 </form>
             </div>
         )
