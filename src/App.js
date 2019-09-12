8cfbae5f5d75d4a004408be5a283f94a246b8411
Glen Pegado <glenpegado@gmail.com>
2019-09-12 17:12:20 -0500

    added styles to checkout page, finalized

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import CheckoutPage from './pages/checkout/chekout.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 
 //Header
 import Header from './components/header/header.component';
