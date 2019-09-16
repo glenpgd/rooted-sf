13c485d83595102b2555d16a8683e2ba92e368bf
Glen Pegado <glenpegado@gmail.com>
2019-09-16 11:30:48 -0500

    created respective selectors and updated corresponding components with new erdux flow for directoty/shop

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -11,16 +11,19 @@ const COLLECTION_ID_MAP = {
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
-
     [selectShop],
     shop => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
 )
 
-export const selectCollection = collectionUrlParam =>
+export const selectCollection = collectionUrlParam => {
     createSelector (
         [selectCollections],
         collections => 
-            collections.find(
-                collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-            )
+           collections[collectionUrlParam]
     )
+}
