3635588025e7a1477c751f2472633da43d00f71e
Glen Pegado <glenpegado@gmail.com>
2019-09-15 22:40:09 -0500

    imported route into shop component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 
 import CollectionOverview from '../../components/collections-overview/collections-overview.component'
 
 const ShopPage = ({collections}) => (
     <div className='shop-page'> 
-        <CollectionOverview/>>
+        <Route component={CollectionOverview} />
     </div>
 )
 
