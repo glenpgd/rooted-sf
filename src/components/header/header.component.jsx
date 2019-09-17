74f1ee9fd6f8df63efb442512f3373f840a20a50
Glen Pegado <glenpegado@gmail.com>
2019-09-17 12:32:12 -0500

    Added more styled components to custom button

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import {createStructuredSelector} from 'reselect'
 import { auth } from '../../firebase/firebase.utils';
@@ -8,13 +7,11 @@ import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 import {selectCartHidden} from '../../redux/cart/cart.selectors';
 import {selectCurrentUser} from '../../redux/user/user.selectors';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-import './header.styles.scss';
 
 import {
   HeaderContainer,
   LogoContainer, 
   OptionsContainer, 
-  OptionDiv, 
   OptionLink
 } from './header.styles';
 
@@ -34,9 +31,9 @@ const Header = ({ currentUser, hidden }) => (
       </OptionLink>
       
       {currentUser ? (
-        <OptionDiv onClick={() => auth.signOut()}>
+        <OptionLink as='div' onClick={() => auth.signOut()}>
           SIGN OUT
-        </OptionDiv>
+        </OptionLink>
       ) : (
         <OptionLink to='/signin'>
           SIGN IN
