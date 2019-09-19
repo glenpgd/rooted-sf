cbf16170e1909761436cd9869bbd945b26c412c2
Glen Pegado <glenpegado@gmail.com>
2019-09-19 10:27:26 -0500

    implemetn code that pulls in our shop data and stores it into the shop reducer from our shop page. Also included are WithSpinner HOC

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -28,11 +28,15 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    collectionRef.get().then(snapshot => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      updateCollections(collectionsMap);
-      this.setState({ loading: false });
-    });
+    fetch('https://firestore.googleapis.com/v1/projects/rooted-sf-db/databases/(default)/documents/collections')
+      .then(response => response.json())
+      .then(collections => collections.this.setState({loading: false}))
+
+    // collectionRef.get().then(snapshot => {
+    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //   updateCollections(collectionsMap);
+    //   this.setState({ loading: false });
+    // });
   }
 
   render() {
