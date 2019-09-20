219f6de58cc21c38398c7ed63e2d78c4b6bbad06
Glen Pegado <glenpegado@gmail.com>
2019-09-20 13:38:42 -0500

    Updated shop saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,9 +1,39 @@
-import {takeEvery} from 'redux-saga/effects';
+import {takeEvery, call, put} from 'redux-saga/effects';
+
+
+import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils'
+
+import {
+    fetchCollectionsSuccess,
+    fetchCollectionsFailure
+} from './shop.actions'
 
 import ShopActionTypes from './shop.types';
 
 export function* fetchCollectionsAsync(){
     yield console.log('Iam fired')
+
+    try {
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(convertCollectionsSnapshotToMap)(snapshot)
+        
+        yield put(fetchCollectionsSuccess(collectionsMap))
+
+    }
+    catch (error){
+        yield put(fetchCollectionsFailure(error.message))
+    }
+
+
+
+    // collectionRef
+    //   .get()
+    //   .then(snapshot => {
+    //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //     dispatch(fetchCollectionsSuccess(collectionsMap));
+    //   })
+    //   .catch(error => dispatch(fetchCollectionsFailure(error.message)));
 }
 
 
