c16832d142b2b17725fc52856dce388d94427cb4
Glen Pegado <glenpegado@gmail.com>
2019-09-24 12:25:35 -0500

    finshed setting up basic node js code

--- server.js
+++ server.js
@@ -4,3 +4,28 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 if(process.env.NODE.ENV !== 'production') require('dotnev').config();
+
+const app = expres();
+const port = process.env.PORT || 5000;
+
+//Convert requests to json
+app.use(bodyParser.json());
+//Helps keep urls strict
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Cross Origin Request (Allows to prperly make requests to abcnk end from local 3000)
+app.use(cors());
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    })
+}
+
+app.listen(port, error => {
+    if(error) throw error;
+    console.log('Server running on port' + port)
+})
