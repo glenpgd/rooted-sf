4a93395d2c9a66bb0688248997f3616e8ff828cc
Glen Pegado <glenpegado@gmail.com>
2019-09-24 12:07:57 -0500

    Moved project to cleint side, created server file for backend

--- server.js
+++ server.js
@@ -2,3 +2,5 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+
+if(process.env.NODE.ENV !== 'production') require('dotnev').config();
