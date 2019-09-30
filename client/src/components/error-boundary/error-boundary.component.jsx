56664c19addcd2331ee274dc874d90ab7b2c7e7e
Glen Pegado <glenpegado@gmail.com>
2019-09-29 23:33:44 -0500

    Added new yarn lock

---
+++ client/src/components/error-boundary/error-boundary.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ErrorBoundary extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            hasErrored: false
+        }
+    }
+    static getDerivedStateFromError(error){
+        
+        return{ }
+    }
+}
+
+e
