5a6b3322c064809f765265ad206d365a433bb3f2
Glen Pegado <glenpegado@gmail.com>
2019-10-01 11:50:52 -0500

    fixed serviceworker get request

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 // const http = require('http');
-const enforce = require('express-sslify')
+// const enforce = require('express-sslify')
+
 if(process.env.NODE.ENV !== 'production') require('dotenv').config();
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -18,15 +18,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 //Cross Origin Request (Allows to properly make requests to the backend from local 3000)
 
-app.use(enforce.HTTPS({ trustAzureHeader: true }));
+// app.use(enforce.HTTPS({ trustAzureHeader: true }));
 
 // http.createServer(app).listen(app.get('port'), function() {
 //   console.log('Express server listening on port ' + app.get('port'));
 // });
 
-app.get('/service-worker.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js')); 
-});
+// app.get('/service-worker.js', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js')); 
+// });
 
 
 app.use(cors());
