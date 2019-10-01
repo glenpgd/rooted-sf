863a639ead4bab380f002fd61bf124ad11c80a9c
Glen Pegado <glenpegado@gmail.com>
2019-10-01 11:39:47 -0500

    fixed serviceworker get request

--- server.js
+++ server.js
@@ -2,34 +2,48 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const enforce = require('express-sslify');
+const http = require('http');
+const enforce = require('express-sslify')
 if(process.env.NODE.ENV !== 'production') require('dotenv').config();
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 //Convert requests to json
 app.use(bodyParser.json());
+
 //Helps keep urls strict
 app.use(bodyParser.urlencoded({extended: true}));
 //Cross Origin Request (Allows to properly make requests to the backend from local 3000)
-app.use(enforce(HTTPS({trustProtoHeader: true})));
+
+app.use(enforce.HTTPS({ trustAzureHeader: true }));
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js')); 
+});
+
+
 app.use(cors());
+
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('*', function(req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
     })
 }
+
 app.listen(port, error => {
     if(error) throw error;
     console.log(`Server running on port ${port}`)
 })
 
-app.get('./service-worker.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'))  
-})
+
 
 app.post('/payment', (req, res) => {
     const body = {
